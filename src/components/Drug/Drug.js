c569740e4b53f407c034112aeb7dc096a89360e1
Omar Segura <omarseguvi@gmail.com>
2018-06-02 14:42:03 -0600

    Drugs now are able to be storaged

--- src/components/Drug/Drug.js
+++ src/components/Drug/Drug.js
@@ -1,10 +1,91 @@
 import React, { Component } from 'react';
+import { Form, FormGroup, Col, Button, ControlLabel, FormControl, Alert } from 'react-bootstrap';
+import { localhost } from '../../connections/connections';
 
 class Drug extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isError: false,
+      errorMessage: '',
+      isPostSuccessful: false,
+      show: true
+    }
+
+    this.handleDismiss = this.handleDismiss.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleDismiss() {
+    this.setState({ show: false });
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const APIurl = localhost + "DBHandler.php/Drug/";
+    
+    var drug = {};
+    
+    data.forEach(function(value, key){
+      drug[key] = value;
+    });
+
+    var json = JSON.stringify(drug);
+    console.log(json);
+    fetch(APIurl, {
+      method: 'POST',
+      body: json
+    }).then(response => this.clearForm())
+      .then( _ => this.setState({isPostSuccessful: true, show:true}))
+      .catch(error => this.setState({isError: true,errorMessage: error.message, show:true}));
+  }
+  
+  clearForm(){
+    document.getElementById("drugForm").reset();
+  }
+
   render() {
+    var alertMessage = '';
+    if(this.state.isError){
+      alertMessage = (<Alert bsStyle="danger" onDismiss={this.handleDismiss}>
+                        <h4>There is an error!</h4>
+                        <p>{this.state.errorMessage}</p>
+                      </Alert>);
+    }
+    else if(this.state.isPostSuccessful){
+      alertMessage = (
+        <Alert bsStyle="success" onDismiss={this.handleDismiss}>
+          <h4>The drug was successfully saved</h4>
+        </Alert>
+      );
+    }
+    
+    if(!this.state.show){
+      alertMessage = '';
+    }
     return (
       <div>
-        Drug
+        {alertMessage}
+        <h2>Add New Drug</h2>
+        <Form id="drugForm" onSubmit={this.handleSubmit} horizontal>
+        <FormGroup>
+            <Col componentClass={ControlLabel} sm={2}>
+              Drug Name
+            </Col>
+            <Col sm={8}>
+              <FormControl id="DrugNm" name="DrugNm" type="text" placeholder="Drug Name" />
+            </Col>
+          </FormGroup>
+          <FormGroup>
+            <FormControl type="hidden" id="isUpdate" name="isUpdate" value="false"/>
+          </FormGroup>
+          <FormGroup>
+            <Col smOffset={1} sm={3}>
+              <Button bsStyle="primary" type="submit">Add Patient</Button>
+            </Col>
+          </FormGroup>
+        </Form>
       </div>
     );
   }
