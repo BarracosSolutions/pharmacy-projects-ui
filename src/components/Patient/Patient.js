34d3f9f231d8f173d9f4bb22b5288c95daea4e69
Omar Segura <omarseguvi@gmail.com>
2018-06-02 13:53:06 -0600

    Patients now are able to be storaged

--- src/components/Patient/Patient.js
+++ src/components/Patient/Patient.js
@@ -1,10 +1,107 @@
 import React, { Component } from 'react';
+import { Form, FormGroup, Col, Button, ControlLabel, FormControl, Alert } from 'react-bootstrap';
+import { localhost } from '../../connections/connections';
 
 class Patient extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isError: false,
+      errorMessage: '',
+      isPostSuccessful: false,
+      show: true
+    }
+
+    this.handleDismiss = this.handleDismiss.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleDismiss() {
+    this.setState({ show: false });
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const APIurl = localhost + "DBHandler.php/Patient/";
+    
+    var patient = {};
+    
+    data.forEach(function(value, key){
+      patient[key] = value;
+    });
+
+    var json = JSON.stringify(patient);
+    console.log(json);
+    fetch(APIurl, {
+      method: 'POST',
+      body: json
+    }).then(response => this.clearForm())
+      .then( _ => this.setState({isPostSuccessful: true, show:true}))
+      .catch(error => this.setState({isError: true,errorMessage: error.message, show:true}));
+  }
+  
+  clearForm(){
+    document.getElementById("patientForm").reset();
+  }
+
   render() {
+    var alertMessage = '';
+    if(this.state.isError){
+      alertMessage = (<Alert bsStyle="danger" onDismiss={this.handleDismiss}>
+                        <h4>There is an error!</h4>
+                        <p>{this.state.errorMessage}</p>
+                      </Alert>);
+    }
+    else if(this.state.isPostSuccessful){
+      alertMessage = (
+        <Alert bsStyle="success" onDismiss={this.handleDismiss}>
+          <h4>The patient was successfully saved</h4>
+        </Alert>
+      );
+    }
+    
+    if(!this.state.show){
+      alertMessage = '';
+    }
+
     return (
       <div>
-        Patient
+        {alertMessage}
+        <h2>Add New Patient</h2>
+        <Form id="patientForm" onSubmit={this.handleSubmit} horizontal>
+        <FormGroup>
+            <Col componentClass={ControlLabel} sm={2}>
+              First Name
+            </Col>
+            <Col sm={8}>
+              <FormControl id="PatientFirtsNm" name="PatientFirtsNm" type="text" placeholder="FirstNm" />
+            </Col>
+          </FormGroup>
+
+          <FormGroup>
+            <Col componentClass={ControlLabel} sm={2}>
+              Last Name
+            </Col>
+            <Col sm={8}>
+              <FormControl id="PatientLastNm" name="PatientLastNm" type="text" placeholder="LastNm" />
+            </Col>
+          </FormGroup>
+          <FormGroup>
+            <Col componentClass={ControlLabel} sm={2}>Medication Description</Col>
+            <Col sm={8}>
+              <FormControl id="MedicationDescription" name="MedicationDescription" componentClass="textarea" placeholder="Medication Description" />
+            </Col>
+          </FormGroup>
+          <FormGroup>
+            <FormControl type="hidden" id="isUpdate" name="isUpdate" value="false"/>
+          </FormGroup>
+          <FormGroup>
+            <Col smOffset={1} sm={3}>
+              <Button bsStyle="primary" type="submit">Add Patient</Button>
+            </Col>
+          </FormGroup>
+        </Form>
       </div>
     );
   }
