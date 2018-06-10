59e18b876e61240b68367ce57e375da1d2bc7617
Omar Segura <omarseguvi@gmail.com>
2018-06-10 13:17:55 -0600

    edit projects and show them

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Col,Table,Button } from 'react-bootstrap';
+import { Row, Col,Table,Button,Modal, Form, FormControl,FormGroup, ControlLabel } from 'react-bootstrap';
 import { localhost } from '../../connections/connections';
 
 class Projects extends Component {
@@ -20,12 +20,17 @@ class Projects extends Component {
     this.state = {
       user: user,
       isUserAut: isAuthenticated,
+      showModal: false,
+      editProject: {},
       directorProjects: [],
       collaboratorProjects: []
     }
 
     this.getDirectorProjects = this.getDirectorProjects.bind(this);
     this.deleteProject = this.deleteProject.bind(this);
+    this.showEditProject = this.showEditProject.bind(this);
+    this.handleModalHide = this.handleModalHide.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount(){
@@ -49,6 +54,42 @@ class Projects extends Component {
       .catch(error => console.log(error));
   }
 
+  handleSubmit(event){
+    event.preventDefault();
+    console.log("entre al updateProject")
+    const data = new FormData(event.target);
+    console.log(data);
+    const APIurl = localhost + "DBHandler.php/Project/";
+    var project = {};
+    
+    console.log(data);
+
+    data.forEach(function(value, key){
+      project[key] = value;
+    });
+
+    var json = JSON.stringify(project);
+    console.log(json);
+    fetch(APIurl, {
+      method: 'POST',
+      body: json
+    }).then(response => console.log(response))
+      .then( _ => this.setState({showModal: false,editProject: {}, directorProjects: []}))
+      .then( _ => this.getDirectorProjects())
+      .catch(error => console.log(error));
+  }
+
+  showEditProject(project){
+    this.setState({showModal: true, editProject: project});
+  }
+
+  handleModalHide() {
+    this.setState({ showModal: false, editProject: {}, directorProjects: []});
+    this.getDirectorProjects();
+  }
+
+  
+
   getDirectorProjects(){
     const APIurl = localhost + "DBHandler.php/Director_Projects/" + this.state.user.EmployeeId;
     fetch(APIurl).then(response => response.json())
@@ -60,14 +101,15 @@ class Projects extends Component {
 
     let projects = "";
     if(this.state.directorProjects.length > 0){
-      console.log(this.state.directorProjects);
       projects = this.state.directorProjects.map(e => 
           <tr key={e.ProjectId}>
             <td>{e.ProjectNm}</td>
+            <td>{e.DrugNm}</td>
+            <td>{e.PatientNationalId} - {e.PatientLastNm},{e.PatientFirstNm}</td>
             <td>{e.Funds}</td>
             <td>{e.Regime}</td>
-            <td>{e.ProjectStatusId}</td>
-            <td><Button bsStyle="primary">Edit</Button><Button onClick={this.deleteProject.bind(this,e.ProjectId)} bsStyle="danger">Delete</Button></td>
+            <td>{e.StatusNm}</td>
+            <td><Button bsStyle="primary" onClick={this.showEditProject.bind(this,e)}>Edit</Button><Button onClick={this.deleteProject.bind(this,e.ProjectId)} bsStyle="danger">Delete</Button></td>
           </tr>
       )
     }
@@ -81,6 +123,8 @@ class Projects extends Component {
               <thead>
                 <tr>
                   <th>Project Name</th>
+                  <th>Drug Name</th>
+                  <th>Patient Name</th>
                   <th>Funds</th>
                   <th>Regime</th>
                   <th>Status</th>
@@ -93,6 +137,101 @@ class Projects extends Component {
             </Table>
           </Col>
         </Row>
+        <Modal bsSize="large" {...this.props} show={this.state.showModal} onHide={this.handleModalHide} dialogClassName="custom-modal">
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-lg">
+              Edit Project
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form id="editProjectForm" onSubmit={this.handleSubmit} horizontal>
+              <FormGroup>
+                <FormControl type="hidden" id="ProjectId" name="ProjectId" value={this.state.editProject.ProjectId}/>
+              </FormGroup>
+              <FormGroup>
+                <Col componentClass={ControlLabel} sm={2}>
+                  Project Name
+                </Col>
+                <Col sm={8}>
+                  <FormControl id="ProjectNm" name="ProjectNm" defaultValue={this.state.editProject.ProjectNm} type="text" placeholder="Project Name" />
+                </Col>
+              </FormGroup>
+              <FormGroup controlId="formControlsSelect">
+                <Col componentClass={ControlLabel} sm={2}>
+                  <ControlLabel> Project Status: </ControlLabel>
+                </Col>
+                <Col sm={8}>
+                  <FormControl name="ProjectStatusId" componentClass="select">
+                    <option value="1">Pending</option>
+                    <option value="2">In Progress</option>
+                    <option value="3">Finished</option>
+                  </FormControl>
+                </Col>
+              </FormGroup>
+              <FormGroup controlId="formControlsSelect">
+                <Col componentClass={ControlLabel} sm={2}>
+                  <ControlLabel >Project Manager</ControlLabel>
+                </Col>
+                <Col sm={8}>
+                  <FormControl name="DirectorId" componentClass="select" placeholder="select">
+                    <option value={this.state.editProject.DirectorId}>{this.state.editProject.DirectorNationalId} - {this.state.editProject.EmployeeLastNm},{this.state.editProject.EmployeeFirstNm}</option>
+                  </FormControl>
+                </Col>
+              </FormGroup>
+              <FormGroup controlId="formControlsSelect">
+                <Col componentClass={ControlLabel} sm={2}>
+                  <ControlLabel >Drug</ControlLabel>
+                </Col>
+                <Col sm={8}>
+                  <FormControl name="DrugId" componentClass="select" placeholder="select">
+                    <option value={this.state.editProject.DrugId}>{this.state.editProject.DrugNm}</option>
+                  </FormControl>
+                </Col>
+              </FormGroup>
+              <FormGroup controlId="formControlsSelect">
+                <Col componentClass={ControlLabel} sm={2}>
+                  <ControlLabel >Patient</ControlLabel>
+                </Col>
+                <Col sm={8}>
+                  <FormControl name="PatientId" componentClass="select" placeholder="select">
+                    <option value={this.state.editProject.PatientId}>{this.state.editProject.PatientNationalId} - {this.state.editProject.PatientLastNm},{this.state.editProject.PatientFirstNm}</option>
+                  </FormControl>
+                </Col>
+              </FormGroup>
+              <FormGroup>
+                <Col componentClass={ControlLabel} sm={2}>
+                  Funds
+                </Col>
+                <Col sm={8}>
+                  <FormControl id="Funds" name="Funds" defaultValue={this.state.editProject.Funds} type="text" placeholder="Funds" />
+                </Col>
+              </FormGroup>
+              <FormGroup>
+                <Col componentClass={ControlLabel} sm={2}>Regime Description</Col>
+                <Col sm={8}>
+                  <FormControl id="Regime" name="Regime" componentClass="textarea" defaultValue={this.state.editProject.Regime} placeholder="Regime Description" />
+                </Col>
+              </FormGroup>
+              <FormGroup>
+                <Col componentClass={ControlLabel} sm={2}>Final Report</Col>
+                <Col sm={8}>
+                  <FormControl id="Report" name="Report" componentClass="textarea" defaultValue={this.state.editProject.Report} placeholder="Final Report" />
+                </Col>
+              </FormGroup>
+              <FormGroup>
+                <FormControl type="hidden" id="isUpdate" name="isUpdate" value="true"/>
+              </FormGroup>
+              <FormGroup>
+                <Col smOffset={1} sm={3}>
+                  <Button bsStyle="primary" type="submit">Edit Project</Button>
+                </Col>
+              </FormGroup>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.handleModalHide}>Close</Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     );
   }
